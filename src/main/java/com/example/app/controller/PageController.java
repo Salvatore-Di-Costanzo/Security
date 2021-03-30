0b9762cda1df7900b21ab95e7804bf4359b1f992
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2021-03-30 11:21:15 +0200

    Separazione controllers

--- src/main/java/com/example/app/controller/PageController.java
+++ src/main/java/com/example/app/controller/PageController.java
@@ -1,7 +1,5 @@
 package com.example.app.controller;
 
-import com.example.app.model.Bambino;
-import com.example.app.model.Utente;
 import com.example.app.service.BambinoService;
 import com.example.app.service.UtenteService;
 import lombok.extern.slf4j.Slf4j;
@@ -9,26 +7,25 @@ import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
-import java.util.List;
-
-@RestController
+@Controller
+@CrossOrigin
 @Slf4j
-public class UtentiController {
-
+public class PageController {
 
     private final UtenteService utenteService;
 
     private final BambinoService bambinoService;
 
     @Autowired
-    private UtentiController(UtenteService utenteService, BambinoService bambinoService) {
+    private PageController(UtenteService utenteService, BambinoService bambinoService) {
         this.utenteService = utenteService;
         this.bambinoService = bambinoService;
     }
@@ -44,47 +41,17 @@ public class UtentiController {
     public String home(Model model, HttpServletRequest request) {
 
         KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) request.getUserPrincipal();
-        KeycloakPrincipal principal=(KeycloakPrincipal)token.getPrincipal();
+        KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
         KeycloakSecurityContext session = principal.getKeycloakSecurityContext();
         String name = session.getToken().getPreferredUsername();
 
         model.addAttribute("name", name);
-        model.addAttribute("email",session.getToken().getPhoneNumber());
+        model.addAttribute("email", session.getToken().getPhoneNumber());
         return "home";
     }
 
     @GetMapping("/sub")
-    public String subscription(Model m){
+    public String subscription(Model m) {
         return "subscription";
     }
-
-    @GetMapping("/utenti")
-    @ResponseBody
-    public List<Utente> getAllUtenti() {
-        return utenteService.getUtenti();
-    }
-
-    @GetMapping("/bambini")
-    @ResponseBody
-    public List<Bambino> getAllBambini() {
-        return bambinoService.getAllBambini();
-    }
-
-    @PostMapping("/userByBimbo")
-    @ResponseBody
-    public Utente getUtenteByBimbo(@PathParam("id") int id){
-        return bambinoService.getUtente(id);
-    }
-
-    @PostMapping("/punteggioUtente")
-    @ResponseBody
-    public int getPunteggioUtente(@PathParam("email") String email) { return utenteService.getPunteggioUtente(email); }
-
-    @PostMapping("/setPunteggio")
-    @ResponseBody
-    public void setPunteggioUtente(@PathParam("email") String email, @PathParam("punteggio") int punteggio){
-        utenteService.setPunteggioUtente(email,punteggio);
-    }
-
-
 }
